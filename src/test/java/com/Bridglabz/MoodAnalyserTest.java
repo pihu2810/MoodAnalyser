aa65301b73205ee77eccf83925496a7884feddff
pihu2810 <vishaka.kadam01@gmail.com>
2022-04-18 17:13:56 +0530

    add Mood Analyser UC2

--- src/test/java/com/Bridglabz/MoodAnalyserTest.java
+++ src/test/java/com/Bridglabz/MoodAnalyserTest.java
@@ -9,14 +9,21 @@ public class MoodAnalyserTest
 	 @Test
 	    public void testMoodAnalysis_whenMoodIsSad() {
 	    	MoodAnalyser mood = new MoodAnalyser();
-	        String md1 = mood.analyseMood("This is a sad message");
+	        String md1 = mood.analyseMood();
 	        assertEquals(md1, "Sad");
 	    }
 
 	    @Test
 	    public void testMoodAnalysis_whenMoodIsHappy() {
 	    	MoodAnalyser  mood = new MoodAnalyser ();
-	        String md2 = mood.analyseMood("This is a happy message");
+	        String md2 = mood.analyseMood();
 	        assertEquals(md2, "Happy");
+	    }
+	    @Test
+	    public void testMoodAnalysis_whenMoodIsNull() {
+	    	MoodAnalyser mood = new MoodAnalyser(null);
+	        String md3 = mood.analyseMood();
+	        assertEquals(md3,"happy");
+
 	    }
 }
